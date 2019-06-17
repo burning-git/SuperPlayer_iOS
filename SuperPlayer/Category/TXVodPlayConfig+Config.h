28a94ed072802f6708c891fb9e029cd4aafdc91d
burning_git <burning_git@163.com>
2019-06-18 07:56:59 +0800

    修改superplay

---
+++ SuperPlayer/Category/TXVodPlayConfig+Config.h
@@ -0,0 +1,16 @@
+//
+//  TXVodPlayConfig+Config.h
+//  SuperPlayer
+//
+//  Created by git burning on 2019/6/18.
+//
+
+#import <TXLiteAVSDK_Professional/TXLiteAVSDK_Professional.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TXVodPlayConfig (Config)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SuperPlayer/Category/TXVodPlayConfig+Config.m
@@ -0,0 +1,12 @@
+//
+//  TXVodPlayConfig+Config.m
+//  SuperPlayer
+//
+//  Created by git burning on 2019/6/18.
+//
+
+#import "TXVodPlayConfig+Config.h"
+
+@implementation TXVodPlayConfig (Config)
+
+@end
